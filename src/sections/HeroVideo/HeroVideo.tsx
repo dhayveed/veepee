98bf6d696bd55928750df38d35f4ba38044f91df
dhayveed <askdhayve@gmail.com>
2026-06-10 15:01:33 +0100

    debug vid

--- src/sections/HeroVideo/HeroVideo.tsx
+++ src/sections/HeroVideo/HeroVideo.tsx
@@ -81,6 +81,8 @@ export default function HeroVideo() {
               muted
               playsInline
               preload="auto"
+              autoPlay
+              webkit-playsinline="true"
             />
           </div>
 
